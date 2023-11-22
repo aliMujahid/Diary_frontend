b14047af64871648d5998c1b0885cd6a97db0174
aliMujahid <mhmmdalimujahid@gmail.com>
2023-11-23 02:48:09 +0600

    react-router init with homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
